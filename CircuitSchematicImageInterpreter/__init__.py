9ec10a74797d2991fb8fcc9a037e3aee84ee31a6
C-R-Kelly <88412292+C-R-Kelly@users.noreply.github.com>
2023-12-02 16:36:24 +0000

    Update __init__.py

--- CircuitSchematicImageInterpreter/__init__.py
+++ CircuitSchematicImageInterpreter/__init__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-MPhil Project Final projectFinal
-=========================
+Circuit Schematic Image Interpreter
+===================================
 
 
 """
 
-__title__ = 'MPhil Project Final projectFinal'
+__title__ = 'Circuit Schematic Image Interpreter'
 __version__ = 'projectFinal'
 __author__ = 'C. R. Kelly'
 __email__ = 'CK598@cam.ac.uk'
+__version__ = 'v1.0.0'
