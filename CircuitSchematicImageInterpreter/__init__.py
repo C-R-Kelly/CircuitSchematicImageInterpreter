8356b9a135221588843b874dcd22691c25fb9470
C-R-Kelly <88412292+C-R-Kelly@users.noreply.github.com>
2023-12-02 16:37:07 +0000

    Update __init__.py

--- CircuitSchematicImageInterpreter/__init__.py
+++ CircuitSchematicImageInterpreter/__init__.py
@@ -7,7 +7,7 @@ Circuit Schematic Image Interpreter
 """
 
 __title__ = 'Circuit Schematic Image Interpreter'
-__version__ = 'projectFinal'
+__version__ = 'v1.0.0'
 __author__ = 'C. R. Kelly'
 __email__ = 'CK598@cam.ac.uk'
-__version__ = 'v1.0.0'
+
